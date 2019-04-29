eb447e128f26b51ddc5f8b3bfcdff1119ebf7e94
David Davis <daviddavis@redhat.com>
2019-04-29 13:14:25 -0400

    Update tests based on publication changes
    ref #4720
    https://pulp.plan.io/issues/4720
    
    Required PR: https://github.com/pulp/pulpcore-plugin/pull/93
    Required PR: https://github.com/pulp/pulpcore/pull/103/files
    Required PR: https://github.com/pulp/pulp_file/pull/207

--- pulp_certguard/tests/functional/utils.py
+++ pulp_certguard/tests/functional/utils.py
@@ -1,4 +1,4 @@
 """Utilities for tests for the certguard plugin."""
 from pulp_file.tests.functional.utils import (  # noqa:F401
-    gen_file_publisher, gen_file_remote, get_file_content_paths
+    create_file_publication, gen_file_publisher, gen_file_remote, get_file_content_paths
 )
