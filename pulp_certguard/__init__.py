545fe25ba2f26a7419f516811645dcf4a63a706f
Brian Bouterse <bmbouter@redhat.com>
2019-11-14 14:03:08 -0500

    Create version 0.1.0rc3
    [noissue]

--- pulp_certguard/__init__.py
+++ pulp_certguard/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.1.0rc3.dev'
+__version__ = '0.1.0rc3'
 
 default_app_config = 'pulp_certguard.app.PulpCertGuardPluginAppConfig'
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 requirements = [
     'PyOpenSSL',
-    'pulpcore~=3.0rc7',
+    'pulpcore>=3.0.0rc8,<3.1',
 ]
 
 with open('README.rst') as f:
@@ -12,7 +12,7 @@ with open('README.rst') as f:
 
 setup(
     name='pulp-certguard',
-    version='0.1.0rc3.dev',
+    version='0.1.0rc3',
     description='X.509 Certguards plugin for the Pulp Project',
     long_description=long_description,
     license='GPLv2+',
