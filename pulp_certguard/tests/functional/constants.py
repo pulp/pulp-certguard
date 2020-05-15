4b48717ba272c4bd53591c556dfd85cac1cf05c0
Brian Bouterse <bmbouter@redhat.com>
2020-05-15 15:47:01 -0400

    Adjust RHSM cert checking to only use base_path
    The `RHSMCertGuard` was requiring the `settings.CONTENT_PATH_PREFIX` to
    also be one of the authorized urls in the certificate. This is not
    desirable due to compatability with a huge number of certificates
    already deployed to clients.
    
    This PR removes the `settings.CONTENT_PATH_PREFIX` from the
    `request.path` so `RHSMCertGuard` authorization check. Now only the
    `Distribution.base_path` is expected in the client RHSM certificate.
    
    The test are also updated with new certificates and base_paths due to
    authorized urls changing for RHSMCertGuard.
    
    https://pulp.plan.io/issues/6694
    closes #6694

--- pulp_certguard/tests/functional/constants.py
+++ pulp_certguard/tests/functional/constants.py
@@ -21,7 +21,6 @@ RHSM_CA_CERT_FILE_PATH = os.path.join(
     _CURRENT_DIR, 'artifacts', 'rhsm', 'katello-default-ca.crt'
 )
 
-
 RHSM_CLIENT_CERT_FROM_UNTRUSTED_CA = os.path.join(
     _CURRENT_DIR, 'artifacts', 'rhsm', 'untrusted_cert.pem'
 )
@@ -31,42 +30,43 @@ RHSM_CLIENT_CERT_TRUSTED_BUT_EXPIRED = os.path.join(
 )
 
 
-# Uber cert path: /pulp
+# Uber cert path: /Default_Organization
 RHSM_UBER_CLIENT_CERT = os.path.join(
     _CURRENT_DIR, 'artifacts', 'rhsm', 'uber.cert'
 )
-RHSM_UBER_CERT_BASE_PATH_ONE = "my-content-view"
-RHSM_UBER_CERT_BASE_PATH_TWO = "another-content-view"
+RHSM_UBER_CERT_BASE_PATH_ONE = "Default_Organization/my-content-view"
+RHSM_UBER_CERT_BASE_PATH_TWO = "Default_Organization/another-content-view"
 
 
-# Zero_var path: /pulp/content/my-content-view/custom/custom_product/foo
+# Zero_var path: /Default_Organization/Library/custom/foo/foo
 RHSM_V1_ZERO_VAR_CLIENT_CERT = os.path.join(
-    _CURRENT_DIR, 'artifacts', 'rhsm', 'v1', '367265616451720695.pem'
+    _CURRENT_DIR, 'artifacts', 'rhsm', 'v1', '159442575569388840.pem'
 )
-RHSM_V1_ZERO_VAR_BASE_PATH = "my-content-view/custom/custom_product/foo"
+RHSM_V1_ZERO_VAR_BASE_PATH = "Default_Organization/Library/custom/foo/foo"
 
 
-# One var path: /pulp/content/my-content-view/content/dist/rhel/server/7/7Server/$basearch/extras/os
+# One var path: /Default_Organization/Library/content/dist/rhel/server/7/7Server/$basearch/extras/os
 # Two var path: /pulp/content/my-content-view/content/dist/rhel/server/7/$releasever/$basearch/os
 RHSM_V1_ONE_AND_TWO_VAR_CLIENT_CERT = os.path.join(
-    _CURRENT_DIR, 'artifacts', 'rhsm', 'v1', '4938399836766610274.pem'
+    _CURRENT_DIR, 'artifacts', 'rhsm', 'v1', '1514454871848760713.pem'
 )
-RHSM_V1_ONE_VAR_BASE_PATH = "my-content-view/content/dist/rhel/server/7/7Server/x86_64/extras/os"
-RHSM_V1_TWO_VAR_BASE_PATH = "my-content-view/content/dist/rhel/server/7/7.4/x86_64/os"
+RHSM_V1_ONE_VAR_BASE_PATH = \
+    "Default_Organization/Library/content/dist/rhel/server/7/7Server/x86_64/extras/os"
+RHSM_V1_TWO_VAR_BASE_PATH = "Default_Organization/Library/content/dist/rhel/server/7/7.4/x86_64/os"
 
 
-# Zero_var path: /pulp/content/my-content-view/custom/custom_product/foo
+# Zero_var path: /Default_Organization/Library/custom/foo/foo
 RHSM_V3_ZERO_VAR_CLIENT_CERT = os.path.join(
-    _CURRENT_DIR, 'artifacts', 'rhsm', 'v3', '6221403274536093008.pem'
+    _CURRENT_DIR, 'artifacts', 'rhsm', 'v3', '4260035510644027985.pem'
 )
-RHSM_V3_ZERO_VAR_BASE_PATH = "my-content-view/custom/custom_product/foo"
+RHSM_V3_ZERO_VAR_BASE_PATH = "Default_Organization/Library/custom/foo/foo"
 RHSM_V3_INVALID_BASE_PATH = "this-is-not-a-valid-base-path"
 
 
-# One var path: /pulp/content/my-content-view/content/dist/rhel8/$releasever/x86_64/baseos/os
-# Two var path: /pulp/content/my-content-view/content/eus/rhel/server/7/$releasever/$basearch/os
+# One var path: /Default_Organization/Library/content/dist/rhel8/$releasever/x86_64/baseos/os
+# Two var path: /Default_Organization/Library/content/dist/rhel/server/7/$releasever/$basearch/os
 RHSM_V3_ONE_AND_TWO_VAR_CLIENT_CERT = os.path.join(
-    _CURRENT_DIR, 'artifacts', 'rhsm', 'v3', '3284681923705053889.pem'
+    _CURRENT_DIR, 'artifacts', 'rhsm', 'v3', '5527980418107729172.pem'
 )
-RHSM_V3_ONE_VAR_BASE_PATH = "my-content-view/content/dist/rhel8/8/x86_64/baseos/os"
-RHSM_V3_TWO_VAR_BASE_PATH = "my-content-view/content/eus/rhel/server/7/7.4/x86_64/os"
+RHSM_V3_ONE_VAR_BASE_PATH = "Default_Organization/Library/content/dist/rhel8/8/x86_64/baseos/os"
+RHSM_V3_TWO_VAR_BASE_PATH = "Default_Organization/Library/content/dist/rhel/server/7/7.4/x86_64/os"
