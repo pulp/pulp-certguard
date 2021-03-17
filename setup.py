38fad37f12e45af50970aedf41d78c53121c00bb
Grant Gainey <ggainey@redhat.com>
2021-03-17 14:49:25 -0400

    Release 1.2.0
    Redmine Query: https://pulp.plan.io/issues?set_filter=1&status_id=*&issue_id=
    
    [noissue]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as f:
 
 setup(
     name='pulp-certguard',
-    version='1.2.0.dev',
+    version='1.2.0',
     description='Certguard plugin for the Pulp Project',
     long_description=long_description,
     license='GPLv2+',
