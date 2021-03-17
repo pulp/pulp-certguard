81160a84b2cc06643d08642371999aa384f73864
Grant Gainey <ggainey@redhat.com>
2021-03-17 14:49:25 -0400

    Bump to 1.2.1.dev
    [noissue]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as f:
 
 setup(
     name='pulp-certguard',
-    version='1.2.0',
+    version='1.2.1.dev',
     description='Certguard plugin for the Pulp Project',
     long_description=long_description,
     license='GPLv2+',
