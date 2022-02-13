4752732855d45cf9910d85b29372eb163bb68ef0
pulpbot <pulp-infra@redhat.com>
2022-02-13 02:46:28 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/script.sh
+++ .github/workflows/scripts/script.sh
@@ -124,7 +124,7 @@ if [ -f $FUNC_TEST_SCRIPT ]; then
   source $FUNC_TEST_SCRIPT
 else
 
-    if [[ "$GITHUB_EVENT_NAME" == "schedule" ]]; then
+    if [[ "$GITHUB_WORKFLOW" == "Certguard Nightly CI/CD" ]]; then
         pytest -v -r sx --color=yes --suppress-no-test-exit-code --pyargs pulp_certguard.tests.functional -m parallel -n 8
         pytest -v -r sx --color=yes --pyargs pulp_certguard.tests.functional -m "not parallel"
     else
