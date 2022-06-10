966d2a4b40f6d24f50f5339f1002ac64f3443cf1
Brian Bouterse <bmbouter@redhat.com>
2022-06-10 09:37:40 -0400

    Adds python package name to AppConfig
    This is a pulpcore 3.20 compatability requirement.
    
    [noissue]

--- pulp_certguard/app/__init__.py
+++ pulp_certguard/app/__init__.py
@@ -7,3 +7,4 @@ class PulpCertGuardPluginAppConfig(PulpPluginAppConfig):
     name = "pulp_certguard.app"
     label = "certguard"
     version = "1.6.0.dev"
+    python_package_name = "pulp-certguard"
