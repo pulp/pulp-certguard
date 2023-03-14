90a7d6bb315659dad3ca9af2526e6da2f61acfe8
pulpbot <pulp-infra@redhat.com>
2023-03-14 20:40:57 +0000

    Bump to 1.6.1.dev
    [noissue]

--- pulp_certguard/app/__init__.py
+++ pulp_certguard/app/__init__.py
@@ -6,6 +6,6 @@ class PulpCertGuardPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_certguard.app"
     label = "certguard"
-    version = "1.6.0"
+    version = "1.6.1.dev"
     python_package_name = "pulp-certguard"
     domain_compatible = True
