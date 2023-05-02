78c65f7b74bc620dcb787d3abeb3aacb399459b3
pulpbot <pulp-infra@redhat.com>
2023-05-02 19:25:12 +0000

    Release 1.6.2 GH Issues:
    [noissue]

--- pulp_certguard/app/__init__.py
+++ pulp_certguard/app/__init__.py
@@ -6,6 +6,6 @@ class PulpCertGuardPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_certguard.app"
     label = "certguard"
-    version = "1.6.2.dev"
+    version = "1.6.2"
     python_package_name = "pulp-certguard"
     domain_compatible = True
