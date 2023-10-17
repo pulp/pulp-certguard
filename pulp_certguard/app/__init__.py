8c7e9a0f0b30d3bab2d12b2bfaa4353e8eceef6c
pulpbot <pulp-infra@redhat.com>
2023-10-17 11:55:09 +0000

    Bump to 1.5.10.dev
    [noissue]

--- pulp_certguard/app/__init__.py
+++ pulp_certguard/app/__init__.py
@@ -6,5 +6,5 @@ class PulpCertGuardPluginAppConfig(PulpPluginAppConfig):
 
     name = 'pulp_certguard.app'
     label = 'certguard'
-    version = '1.5.9'
+    version = '1.5.10.dev'
     python_package_name = "pulp-certguard"
