125aa90366c758bd342f5c67ef33f6773c290fda
pulpbot <pulp-infra@redhat.com>
2024-01-25 14:33:02 +0000

    Bump minor version [noissue]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.rst") as f:
 
 setup(
     name="pulp-certguard",
-    version="1.8.0.dev",
+    version="1.9.0.dev",
     description="Certguard plugin for the Pulp Project (transitional dummy package)",
     long_description=long_description,
     license="GPLv2+",
