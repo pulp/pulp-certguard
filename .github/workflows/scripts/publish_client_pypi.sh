fb222a2435d271fe21792da4040d379fe5b4b87f
pulpbot <pulp-infra@redhat.com>
2024-06-02 02:38:59 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/publish_client_pypi.sh
+++ .github/workflows/scripts/publish_client_pypi.sh
@@ -18,14 +18,3 @@ if [[ -z "$VERSION" ]]; then
   echo "No version specified."
   exit 1
 fi
-
-RESPONSE="$(curl --write-out '%{http_code}' --silent --output /dev/null "https://pypi.org/project/pulp-certguard-client/$VERSION/")"
-
-if [ "$RESPONSE" == "200" ];
-then
-  echo "pulp-certguard client $VERSION has already been released. Skipping."
-  exit
-fi
-
-twine upload -u __token__ -p "$PYPI_API_TOKEN" \
-;
